4366678e99f45dc5be8dc8e9ad183fc2d095a545
Matt Lawson <mlawson1217@gmail.com>
2017-08-23 17:05:12 -0400

    Cleaned to flake8 specs

--- Matt_Testing.py
+++ Matt_Testing.py
@@ -1,24 +1,22 @@
 import requests
 from bs4 import BeautifulSoup
 
-#8-K test URL
-#url = "https://www.sec.gov/Archives/edgar/data/354950/000035495017000017/hd_8kx05242017.htm"
-#10-K Test URLs
-url ="https://www.sec.gov/Archives/edgar/data/0001652044/000165204417000008/goog10-kq42016.htm"
-#url = "https://www.sec.gov/Archives/edgar/data/936468/000119312517210489/d410677d11k.htm"
-#10-Q URL
-#url = "https://www.sec.gov/Archives/edgar/data/354950/000035495017000014/hd_10qx04302017.htm"
-#11-K Test URL
-#url = "https://www.sec.gov/Archives/edgar/data/354950/000035495017000026/hd_prx11kx12312016.htm"
-
-
+# 8-K test URL
+# url = "https://www.sec.gov/Archives/edgar/data/354950/000035495017000017/hd_8kx05242017.htm"
+# 10-K Test URLs
+url = "https://www.sec.gov/Archives/edgar/data/0001652044/000165204417000008/goog10-kq42016.htm"
+# url = "https://www.sec.gov/Archives/edgar/data/936468/000119312517210489/d410677d11k.htm"
+# 10-Q URL
+# url = "https://www.sec.gov/Archives/edgar/data/354950/000035495017000014/hd_10qx04302017.htm"
+# 11-K Test URL
+# url = "https://www.sec.gov/Archives/edgar/data/354950/000035495017000026/hd_prx11kx12312016.htm"
 
 r = requests.get(url, stream=True)
 soup = BeautifulSoup(r.content, 'lxml')
 
 
-
 def CleanText(text):
+    """Cleaning Text."""
     text = text.replace('\u200b', " ")
     text = text.replace('ý', '`check_marked`')
     text = text.replace('• \n', '')
@@ -31,7 +29,9 @@ soup = BeautifulSoup(cleanText, 'lxml')
 
 # For finding number of pages and page locations
 
+
 def Pages(text):
+    """Finding Page Location by HR Tag."""
     PageNumToHR = []
     mydict = {}
     NumPages = 0
@@ -43,24 +43,24 @@ def Pages(text):
             HRTags = HRTags + 1
             try:
                 if pageBreak.previous_sibling.contents[0].text:
-                    #print(pageBreak.previous_sibling.contents[0].text)
+                    # print(pageBreak.previous_sibling.contents[0].text)
                     NumPages = NumPages + 1
-                    PageNumToHR.append([HRTags,NumPages])
+                    PageNumToHR.append([HRTags, NumPages])
                     mydict.update({str(NumPages): str(HRTags)})
                 else:
                     pass
                     q = q + 1
-            except:
+            except Exception:
                 try:
                     if int(pageBreak.find_previous_sibling('p').find_previous_sibling('p').text):
-                        #print("Longer: " + str(pageBreak.find_previous_sibling('p').find_previous_sibling('p').text))
+                        # print("Longer: " + str(pageBreak.find_previous_sibling('p').find_previous_sibling('p').text))
                         NumPages = NumPages + 1
-                        PageNumToHR.append([HRTags,NumPages])
+                        PageNumToHR.append([HRTags, NumPages])
                         mydict.update({str(NumPages): str(HRTags)})
                     else:
                         pass
                         q = q + 1
-                except:
+                except Exception:
                     i = i + 1
                     pass
     return mydict
@@ -70,6 +70,7 @@ pageLoc = Pages(soup)
 # Start of whats needed for Getting Table Of Contents
 
 def GetTableOfContents(text):
+    """Getting Table of Contents."""
     tables = text('table')
     allText = []
     index = []
@@ -86,31 +87,31 @@ def GetTableOfContents(text):
             if int(phrase):
                 numList = []
                 numList.append(phrase)
-                for num in range(d, i-1):
+                for num in range(d, i - 1):
                     numList.append(allText[num])
                 d = i
                 index.append(numList)
-        except:
+        except Exception:
             pass
     return index
 
 TOC = GetTableOfContents(soup)
 
+
 def MakeADict(lists):
+    """Creating Dictionary of Table of Contents Sections and Pages."""
     newDict = {}
     for listy in lists:
-        i = 0
         pageNum = 0
         myKey = []
         listy[:] = [item for item in listy if item != 'Table of Contents']
-        listLen = len(listy)
         for item in listy:
             try:
                 if int(item):
                     pageNum = int(item)
                 else:
                     pass
-            except:
+            except Exception:
                 myKey.append(item)
         for section in myKey:
             newDict.update({str(section): pageNum})
@@ -122,9 +123,12 @@ print(TableOfContents)
 
 
 def FindStartPage(Section):
+    """Finding Start Page of Given Section."""
     return TableOfContents.get(Section, "Section Does Not Exit")
 
+
 def SectionPageBreaks(SectionName):
+    """Finding Location of Start Page of Given Section."""
     pageNum = FindStartPage(SectionName)
     return pageLoc.get(str(pageNum), "Not Found")
 
