42fde6a36e3e5a4e979f40128a716a603e7344d0
Matt Lawson <mlawson1217@gmail.com>
2017-08-23 15:52:10 -0400

    This will return the starting page of whatever section title you give it.

--- Matt_Testing.py
+++ Matt_Testing.py
@@ -4,10 +4,10 @@ from bs4 import BeautifulSoup
 #8-K test URL
 #url = "https://www.sec.gov/Archives/edgar/data/354950/000035495017000017/hd_8kx05242017.htm"
 #10-K Test URLs
-url ="https://www.sec.gov/Archives/edgar/data/0001652044/000165204417000008/goog10-kq42016.htm"
+#url ="https://www.sec.gov/Archives/edgar/data/0001652044/000165204417000008/goog10-kq42016.htm"
 #url = "https://www.sec.gov/Archives/edgar/data/936468/000119312517210489/d410677d11k.htm"
 #10-Q URL
-#url = "https://www.sec.gov/Archives/edgar/data/354950/000035495017000014/hd_10qx04302017.htm"
+url = "https://www.sec.gov/Archives/edgar/data/354950/000035495017000014/hd_10qx04302017.htm"
 #11-K Test URL
 #url = "https://www.sec.gov/Archives/edgar/data/354950/000035495017000026/hd_prx11kx12312016.htm"
 
@@ -15,7 +15,7 @@ url ="https://www.sec.gov/Archives/edgar/data/0001652044/000165204417000008/goog
 
 r = requests.get(url, stream=True)
 soup = BeautifulSoup(r.content, 'lxml')
-#soup = BeautifulSoup(r.text, 'html.parser')
+
 
 
 def CleanText(text):
@@ -68,9 +68,7 @@ def Pages(text):
                 except:
                     i = i + 1
                     pass
-    #print("Try Errors: "+ str(q), "Except Errors: "+ str(i) + ":::", " NumPages: "+ str(NumPages), "'hr' Tags: " + str(HRTags))
-    #print(PageNumToHR)
-    #print(mydict)
+
 
 def GetTableOfContents(text):
     tables = text('table')
@@ -82,7 +80,7 @@ def GetTableOfContents(text):
         if table.find('a'):
             for a in table.findAll('a'):
                 allText.append(a.text)
-    #print(len(allText))
+
     for phrase in allText:
         i = i + 1
         try:
@@ -91,41 +89,38 @@ def GetTableOfContents(text):
                 numList.append(phrase)
                 for num in range(d, i-1):
                     numList.append(allText[num])
-                #print("Page: ", int(phrase), numList)
                 d = i
                 index.append(numList)
         except:
             pass
     return index
 
-LOL = GetTableOfContents(soup)
+TOC = GetTableOfContents(soup)
 
 def MakeADict(lists):
     newDict = {}
     for listy in lists:
         i = 0
-        makingAnother = []
+        pageNum = 0
         myKey = []
         listy[:] = [item for item in listy if item != 'Table of Contents']
         listLen = len(listy)
-        #print(listLen, "tis the season:", listy)
         for item in listy:
             try:
                 if int(item):
-                    makingAnother.append(item)
+                    pageNum = int(item)
                 else:
                     pass
             except:
                 myKey.append(item)
-        newDict.update({str(myKey): makingAnother})
-    print(newDict)
+        for section in myKey:
+            newDict.update({str(section): pageNum})
+    return newDict
 
 
+TableOfContents = MakeADict(TOC)
 
+def FindStartPage(Section):
+    return TableOfContents.get(Section)
 
-
-MakeADict(LOL)
-
-# calling function to retrive name
-#print(CompanyName(soup))
-#Pages(soup)
+print(FindStartPage("Item1."))
